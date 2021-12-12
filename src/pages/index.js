3a7a491c1f0447e5d0f99ac67748a9ad48384715
soarecor <cornell109@gmail.com>
2021-12-12 15:42:05 -0500

    fixing gatsby import error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import About from "../components/about"
+import { graphql } from 'gatsby'
 
 export const query = graphql`
   query {
